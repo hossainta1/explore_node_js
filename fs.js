ce0431b510fcbe3e6dc11ed275dca7fd90c13c88
Tanvir Hossain <tanvir.hossain303030@gmail.com>
2025-05-29 23:15:53 +0600

    Asynchronous way to read and write file

--- fs.js
+++ fs.js
@@ -1,16 +1,26 @@
 // synchronous way to read and write file
 
-const fs = require("fs");
+// const fs = require("fs");
 
 
 
-const text = "learning File System"
-fs.writeFileSync("./hellow.tsxt", text)
-const data = fs.readFileSync("./hellow.tsxt", { encoding: 'utf8' });
+// const text = "learning File System"
+// fs.writeFileSync("./hellow.tsxt", text)
+// const data = fs.readFileSync("./hellow.tsxt", { encoding: 'utf8' });
 
-console.log(data)
+// console.log(data)
 
 
 
 
 // Asynchronous way to read and write file
+
+const fs = require('fs');
+
+fs.readFile('./hellow.tsxt', { encoding: 'utf8' }, (err, data) => {
+    if (err) {
+        console.log("Something went wrong:", err);
+        return;
+    }
+    console.log(data);
+});
