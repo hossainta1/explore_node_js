5561a508c040e83343d826f3c56975bb7a9972d8
Tanvir Hossain <tanvir.hossain303030@gmail.com>
2025-05-30 15:50:41 +0600

    Test API

--- logger-app/todo-app/server.js
+++ logger-app/todo-app/server.js
@@ -1,10 +1,23 @@
 const http = require("http");
+const { url } = require("inspector");
 
-const server = http.createServer((req, res)=> {
-    console.log({req, res})
-   res.end("welcome to TODO app")
+const server = http.createServer((req, res) => {
+    //     console.log(req.url, res.method)
+    //    res.end("welcome to TODO app")
+    if (req.url === "/todos" && req.method === "GET") {
+       res.end("All TODO")
+    } else if(req.url === "/todos/create-todo" && req.method === "POST"){
+       res.end("TODO Created")
+    }
+    else{
+         res.end("Route Not Found")
+    }
 });
 
-server.listen(5000, "127.0.0.1", ()=> {
+server.listen(5000, "127.0.0.1", () => {
     console.log("✅ Server listing on port 5000")
 });
+
+//  /todo - GET - all todo
+//  /todo/create-todo - POST create
+// 
